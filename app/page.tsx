29ed731cedbc34f4e7221ee9bc746e428205704d
Ankush850 <ankushsinghrawat154@gmail.com>
2026-02-27 16:05:11 +0530

    update the project with App folder

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Navbar from '@/components/navbar'
+import Hero from '@/components/hero'
+import ProductEcosystem from '@/components/product-ecosystem'
+import FeaturedProducts from '@/components/featured-products'
+import LifestyleGrid from '@/components/lifestyle-grid'
+
+import Footer from '@/components/footer'
+
+export default function Home() {
+  return (
+    <main className="overflow-hidden">
+      <Navbar />
+      <Hero />
+      <ProductEcosystem />
+      <FeaturedProducts />
+      <LifestyleGrid />
+      
+      <Footer />
+    </main>
+  )
+}
